2bfa9aea7a4eedc733f8af52cd14ca0e5e676a38
araya <arieman40@gmail.com>
2025-07-22 09:49:32 +0300

    First commit

---
+++ src/main/java/com/mvcGitLead/GitmvcApplication.java
@@ -0,0 +1,13 @@
+package com.mvcGitLead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvcGitLead/GitmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvcGitLead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
