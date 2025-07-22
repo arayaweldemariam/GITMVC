e51d7696be943179a9f71f550d4d164306ad0ed0
araya <arieman40@gmail.com>
2025-07-22 14:45:13 +0300

    Second commit

---
+++ src/main/java/com/mvcGitLead/dto/PersonDto.java
@@ -0,0 +1,43 @@
+package com.mvcGitLead.dto;
+
+public class PersonDto {
+
+	private String name;
+	private String lastName;
+	private int userId;
+	
+	
+	public PersonDto(String name, String lastName, int userId) {
+		super();
+		this.name = name;
+		this.lastName = lastName;
+		this.userId = userId;
+	}
+	public PersonDto() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	@Override
+	public String toString() {
+		return "PersonDto [name=" + name + ", lastName=" + lastName + ", userId=" + userId + "]";
+	}
+	
+	
+}
